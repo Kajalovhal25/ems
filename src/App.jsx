1464f0733a8142485819063f1cfdb954f096b506
Kajal Ovhal <ovhalkajal25@gmail.com>
2026-09-18 23:14:21 +0530

    useeffect update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import Login from "./components/Auth/Login";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
 import EmplyoyeeDashboard from "./components/Dashboard/EmplyoyeeDashboard";
+import { useEffect } from "react";
+import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
+import { useState } from "react";
 
 function App() {
+  
+  const [user, setUser] = useState(null);
+
+  const handleLogin =(email,password)=>{  
+    if (email =='admin@me.com' && password == '123'){
+      console.
+    }else if(email == 'employee@me.com' && password == '123'){
+      console.log('employee here');
+    }else{
+    alert('Invalid credentials');
+    }
+  handleLogin('admin@me.com', '123');
+
   return (
-    <div>
-      <Login />
+    <>
+      {!user ? <Login handleLogin={handleLogin} />:''}
       {/*<EmplyoyeeDashboard />*/}
       {/*<AdminDashboard/>*/}
-    </div>
+    </>
   );
-}
-
+  };
+};
 export default App;
